5974807922aa2efbe4ab3a2b81075af74d610782
MiltonMatias22 <nodout.develops@gmail.com>
2018-08-02 12:00:26 -0300

    Categoria: Criação CategoriaService e CategoriaRepository

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -3,26 +3,26 @@ package com.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(cat1);
-		categorias.add(cat2);
-		
-		return categorias;
+		return ResponseEntity.ok().body(categoriaService.buscar(id));
 	}
 }
