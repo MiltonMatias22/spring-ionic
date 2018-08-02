6a30e18e7b93085eb2cd3983d6c50050dfe4e01e
MiltonMatias22 <nodout.develops@gmail.com>
2018-08-02 13:08:39 -0300

    Criando operação de instanciação

--- src/main/java/com/cursomc/SpringIonicApplication.java
+++ src/main/java/com/cursomc/SpringIonicApplication.java
@@ -1,12 +1,36 @@
 package com.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursomc.domain.Categoria;
+import com.cursomc.services.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringIonicApplication {
+public class SpringIonicApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/*Categoria */
+		Categoria cat1 = new Categoria(1, "Informática");		
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		/*------------------- fim categoria ----------------*/
+		
 	}
 }
--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cursomc.domain.Categoria;
 import com.cursomc.services.CategoriaService;
 
 @RestController
