ce5b0afb869d0a79f97061d47090ea93afb4ba47
MiltonMatias22 <nodout.develops@gmail.com>
2018-08-06 17:11:11 -0300

    Categoria: method DELETE

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,12 @@ public class CategoriaResource {
 		
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
+		this.categoriaService.deletar(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursomc/services/CategoriaService.java
@@ -3,9 +3,11 @@ package com.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.services.exception.DataIntegrityException;
 import com.cursomc.services.exception.ObjectNotFoundException;
 import com.cursomc.services.repositories.CategoriaRepository;
 
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria atualizar(Categoria categoria) {
 		buscar(categoria.getId());
 		return this.repository.save(categoria);
+	}
+
+	public void deletar(Integer id) {
+		buscar(id);
+		try {
+			this.repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("ERRO ou remover registro, categoria está vinculada a produtos!");
+		}
 	}
 }
