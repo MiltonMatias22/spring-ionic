e9117982c6cc4bf6cbc336b42adf5d74f9b82d1b
MiltonMatias22 <nodout.develops@gmail.com>
2018-08-06 16:17:29 -0300

    Categoria: method POST

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,18 @@
 package com.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.cursomc.domain.Categoria;
 import com.cursomc.services.CategoriaService;
 
 @RestController
@@ -20,5 +26,16 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
 		
 		return ResponseEntity.ok().body(this.categoriaService.buscar(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria){
+		categoria = this.categoriaService.inserir(categoria);
+		URI uri= ServletUriComponentsBuilder
+							.fromCurrentRequest()
+							.path("/{id}").buildAndExpand(categoria.getId())
+							.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
